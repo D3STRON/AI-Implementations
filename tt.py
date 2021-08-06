1b638b4ec39c4dc6158998b4425f9b9cdf9df1a1
D3STRON <ghoshanurag99@gmail.com>
2021-08-06 08:37:40 +0530

    Update tt.py
    Added comments and cleaned the code

--- tt.py
+++ tt.py
@@ -4,18 +4,19 @@ Classes = 4
 Periods = 4
 F = 0
 mutation_rate = 0.01
-tt = [ [ [ 'n/a' for k in range(Classes) ] for j in range(Periods) ] for i in range(Days) ] 
+tt = [ [ [ 'n/a' for clas in range(Classes) ] for period in range(Periods) ] for day in range(Days) ] 
 subs = ['eng','mr','eco','oc','sp','bk']
 weekDays = ['MonDays','TuesDays','WednesDays','ThursDays','FriDays','SaturDays']
 lec_per_sub_per_clas = (Days*Periods)/len(subs)
 #stores the number of lectures per subject per class will have
-classsubfreq = [[ lec_per_sub_per_clas for i in range(len(subs)) ] for j in range(Classes) ]
+classsubfreq = [[ lec_per_sub_per_clas for sub in range(len(subs)) ] for clas in range(Classes) ]
 
 def sublockerIni(k):
     if k ==2:
-        #sets the frequency of occurance of a subject in a perticular day to zero initially
+        #sets the number of occurance of a subject in a perticular day to zero initially
         return 0
     else:
+        #sets all conflicts of lectures to none initially
         return [ -1 for l in range(Classes) ]
 
 #this data structure tracks the subjects occurance in a day,
@@ -32,19 +33,22 @@ def fitness(ttn, nsublocker):
         for period in range(Periods):
             for clas in range(Classes):
                 subject = ttn[day][period][clas]
+                #the logic around the arraylist d ensures no subject lecture is placed in the begning and in the end of the day(was a requirement of client)
                 if period == 0:
                     d.append(ttn[day][period][clas])
                 elif period == Periods-1 and (ttn[day][period][clas] in d):
                     f-=1
                 elif ttn[day][period][clas] in d:
                     d.remove(ttn[day][period][clas])
+                #if there are same lectures in two class in the same period reduce fitness
                 if nsublocker[subject][day][0][period] >0:
                     f-=1
+                #if there are same lectures in same class consecutively reduce fitness    
                 if nsublocker[subject][day][1][clas] >0:
                     f-=1
+                #if in a day a number of lectures of a subjects is equalt to or greater than the number of periods reduce fitness
                 if nsublocker[subject][day][2]>=Periods:
                     f-=1
-                
     return f
 
 def place(ptt,i,j,k,nsublocker):
@@ -83,18 +87,19 @@ def generateChild(ptt,F):
     f =fitness(ttn,nsublocker)
     return [ttn,f]
                 
-
-for i in range(Days):
-    for j in range(Periods):
-        for k in range(Classes):
+#Generating a initial parent
+for day in range(Days):
+    for period in range(Periods):
+        for clas in range(Classes):
             random_sub = random.randint(0,len(subs)-1)
-            while classsubfreq[j][random_sub]==0:
+            while classsubfreq[period][random_sub]==0:
                 random_sub = random.randint(0,len(subs)-1)
-            sublocker[random_sub][i][0][j] +=1
-            sublocker[random_sub][i][1][k] +=1
-            sublocker[random_sub][i][2]+=1
-            tt[i][j][k]=random_sub
-            classsubfreq[j][random_sub]-=1
+            sublocker[random_sub][day][0][period] +=1
+            sublocker[random_sub][day][1][clas] +=1
+            sublocker[random_sub][day][2]+=1
+            tt[day][period][clas]=random_sub
+            #checks the number of lectures of a subject allotted to a class is within said limits
+            classsubfreq[period][random_sub]-=1
 F = fitness(tt,sublocker)
 while F<0:
     result = generateChild(tt,F)
